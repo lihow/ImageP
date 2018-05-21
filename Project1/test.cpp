4d95a58720bd19d559b1bc70200d229f98fcff28
lihow <2458858175@qq.com>
2018-05-21 11:40:13 +0800

    完善了三帧差法的播放控制，供调试

--- Project1/test.cpp
+++ Project1/test.cpp
@@ -26,6 +26,23 @@ void compute_absolute_mat(const Mat& in, Mat & out)
 	}
 }
 
+Mat image;   //读入视频帧的Mat  
+char* windowName = "Video Control"; //播放窗口名称  
+char* trackBarName = "播放进度";    //trackbar控制条名称  
+double totalFrame = 1.0;     //视频总帧数  
+double currentFrame = 1.0;    //当前播放帧  
+int trackbarValue = 1;    //trackbar控制量  
+int trackbarMax = 255;   //trackbar控制条最大值  
+double frameRate = 1.0;  //视频帧率  
+VideoCapture video;    //声明视频对象  
+double controlRate = 0.1;
+
+void TrackBarFunc(int, void(*))
+{
+	controlRate = (double)trackbarValue / trackbarMax*totalFrame; //trackbar控制条对视频播放进度的控制  
+	video.set(CV_CAP_PROP_POS_FRAMES, controlRate);   //设置当前播放帧  
+}
+
 
 int main(){
 
@@ -38,32 +55,85 @@ int main(){
 //	imshow("src", dst);
 //	waitKey(0);
 
-	string videoPath = "F:\\煤炭识别\\煤炭视频\\煤炭视频3\\哈M13机头入料口 20180417 174000 20180417 204030.avi";
+	string videoPath = "F:\\煤炭识别\\煤炭视频\\煤炭视频3\\ruliaokou-part.avi";
 	string videoPath1 = "F:\\煤炭识别\\煤炭视频\\煤炭视频1\\Mei-part.avi";
 
 
 
 	/*********************************三帧差法检测物体********************************************/
 	
-	VideoCapture videoCap(videoPath);
-	if (!videoCap.isOpened())
+	//原始方法
+	//VideoCapture videoCap(videoPath);
+	//if (!videoCap.isOpened())
+	//{
+	//	return -1;
+	//}
+	//double videoFPS = videoCap.get(CV_CAP_PROP_FPS);  //获取帧率  
+	//double videoPause = 1000 / videoFPS;
+	//Mat framePrePre; //上上一帧  
+	//Mat framePre; //上一帧  
+	//Mat frameNow; //当前帧  
+	//Mat frameDet; //运动物体  
+	//videoCap >> framePrePre;
+	//videoCap >> framePre;
+	//cvtColor(framePrePre, framePrePre, CV_RGB2GRAY);
+	//cvtColor(framePre, framePre, CV_RGB2GRAY);
+	//int save = 0;
+	//while (true)
+	//{
+	//	videoCap >> frameNow;
+	//	if (frameNow.empty() || waitKey(videoPause) == 27)
+	//	{
+	//		break;
+	//	}
+	//	cvtColor(frameNow, frameNow, CV_RGB2GRAY);
+	//	Mat Det1;
+	//	Mat Det2;
+	//	absdiff(framePrePre, framePre, Det1);  //帧差1  
+	//	absdiff(framePre, frameNow, Det2);     //帧差2  
+	//	threshold(Det1, Det1, 0, 255, CV_THRESH_OTSU);  //自适应阈值化  
+	//	threshold(Det2, Det2, 0, 255, CV_THRESH_OTSU);
+	//	Mat element = getStructuringElement(0, Size(3, 3));  //膨胀核  
+	//	dilate(Det1, Det1, element);    //膨胀  
+	//	dilate(Det2, Det2, element);
+	//	bitwise_and(Det1, Det2, frameDet);
+	//	framePrePre = framePre;
+	//	framePre = frameNow;
+	//	imshow("Video", frameNow);
+	//	imshow("Detection", frameDet);
+	//}
+
+	//改进
+	video.open(videoPath);
+	//VideoCapture video(videoPath);
+	if (!video.isOpened())
 	{
 		return -1;
 	}
-	double videoFPS = videoCap.get(CV_CAP_PROP_FPS);  //获取帧率  
+	totalFrame = video.get(CV_CAP_PROP_FRAME_COUNT);  //获取总帧数  
+	double videoFPS = video.get(CV_CAP_PROP_FPS);  //获取帧率  
 	double videoPause = 1000 / videoFPS;
+
+
+	namedWindow(windowName);
+	createTrackbar(trackBarName, windowName, &trackbarValue, trackbarMax, TrackBarFunc);
+	TrackBarFunc(0, 0);
+
 	Mat framePrePre; //上上一帧  
 	Mat framePre; //上一帧  
 	Mat frameNow; //当前帧  
 	Mat frameDet; //运动物体  
-	videoCap >> framePrePre;
-	videoCap >> framePre;
+	Mat frameOri; //未被处理的原始帧
+	video >> framePrePre;
+	video >> framePre;
 	cvtColor(framePrePre, framePrePre, CV_RGB2GRAY);
 	cvtColor(framePre, framePre, CV_RGB2GRAY);
 	int save = 0;
 	while (true)
 	{
-		videoCap >> frameNow;
+		video >> frameNow;
+		frameNow.copyTo(frameOri);
+
 		if (frameNow.empty() || waitKey(videoPause) == 27)
 		{
 			break;
@@ -81,11 +151,12 @@ int main(){
 		bitwise_and(Det1, Det2, frameDet);
 		framePrePre = framePre;
 		framePre = frameNow;
-		imshow("Video", frameNow);
+
+		resize(frameOri, frameOri, Size(500, 400));
+		resize(frameDet, frameDet, Size(500, 400));
+		imshow(windowName, frameOri);
 		imshow("Detection", frameDet);
 	}
-
-
 	
 
 
@@ -132,36 +203,36 @@ int main(){
 	}
 	*/
 
-	/*
-	//方法二 
-	VideoCapture cap;
-	cap.open(videoPath1);
+	//
+	////方法二 
+	//VideoCapture cap;
+	//cap.open(videoPath1);
 
-	if (!cap.isOpened()){
-		std::cout << "视频读取失败！" << std::endl;
-		return -1;
-	}
+	//if (!cap.isOpened()){
+	//	std::cout << "视频读取失败！" << std::endl;
+	//	return -1;
+	//}
 
-	Mat  gray, prvGray, optFlow, absoluteFlow, img_for_show;
-	while (1){
-		cap >> img;
-		if (img.empty()) break;
+	//Mat  gray, prvGray, optFlow, absoluteFlow, img_for_show;
+	//while (1){
+	//	cap >> img;
+	//	if (img.empty()) break;
 
-		cvtColor(img, gray, CV_BGR2GRAY);
-		if (prvGray.data){
-			calcOpticalFlowFarneback(prvGray, gray, optFlow, 0.5, 3, 15, 3, 5, 1.2, 0); //使用论文参数  
-			compute_absolute_mat(optFlow, absoluteFlow);
-			normalize(absoluteFlow, img_for_show, 0, 255, NORM_MINMAX, CV_8UC1);
-			
-			imshow("opticalFlow", img_for_show);
-			imshow("resource", img);
-		}
-		cv::swap(prvGray, gray);
+	//	cvtColor(img, gray, CV_BGR2GRAY);
+	//	if (prvGray.data){
+	//		calcOpticalFlowFarneback(prvGray, gray, optFlow, 0.5, 3, 15, 3, 5, 1.2, 0); //使用论文参数  
+	//		compute_absolute_mat(optFlow, absoluteFlow);
+	//		normalize(absoluteFlow, img_for_show, 0, 255, NORM_MINMAX, CV_8UC1);
+	//		imshow("opticalFlow", img_for_show);
+	//		//imshow("opticalFlow", absoluteFlow);
+	//		imshow("resource", img);
+	//	}
+	//	cv::swap(prvGray, gray);
 
-		waitKey(1);
-	}
+	//	waitKey(1);
+	//}
 
-	*/
+	
 
 
 
